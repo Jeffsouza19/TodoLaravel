a315f594b930c8165899f9599561dce52fbfc947
Jeferson Souza <jeffsouza.edif@gmail.com>
2022-07-03 01:09:15 -0300

    first commit

---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <title>TodoApi</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="sidebar">
+            <a href="">
+                <img src="/assets/images/logo.png" alt="Logo">
+            </a>
+        </div>
+        <div class="content">
+            <nav>
+                <a href="" class="btn btn-primary">Criar Tarefa</a>
+            </nav>
+            <main>
+                <section class="graph">
+                    <div class="graph-header">
+                        <h2>Progresso do dia</h2>
+                        <div class="graph-header-line"></div>
+                        <div class="graph-header--date">
+                            <img src="/assets/images/icon-prev.png" alt="">
+                            12/Dez/2022
+                            <img src="/assets/images/icon-next.png" alt="">
+                        </div>
+                    </div>
+                    <div class="graph-header--subtitle">Tarefas: <b>3/6</b></div>
+                    <div class="graph-placeholder">
+
+                    </div>
+                    <div class="graph--task-left">
+                        <img src="/assets/images/icon-info.png" alt=""> Restam 3 tarefas a serem realizadas
+                    </div>
+                </section>
+                <section class="list">
+                    <div class="list-header">
+                        <select name="" id="" class="list-header--select">
+                            <option value="1">Todas as tarefas</option>
+                        </select>
+                    </div>
+                    <div class="task-list">
+                        <div class="task">
+                            <div class="title">
+                                <input type="checkbox" name="" id="">
+                                <div class="task-title">Titulo da tarefa</div>
+                            </div>
+                            <div class="priority">
+                                <div class="sphere"></div>
+                                <div>Titulo da tarefa</div>
+                            </div>
+                            <div class="actions">
+                                <a href="">
+                                    <img src="/assets/images/icon-edit.png" alt="">
+                                </a>
+                                <a href="">
+                                    <img src="/assets/images/icon-delete.png" alt="">
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </main>
+        </div>
+    </div>
+</body>
+
+</html>
